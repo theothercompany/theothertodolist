f7a6b7c84570b8b1113091613465d0939c9a5c35
black_000 <black_000@hippo>
2015-04-15 09:14:05 -0400

    at-support: The at-controller now has a "/rest/at/populate.json" to load some sample data and "/rest/at/list.json" to get a Map<String, List<Integer>> with the at information.

--- src/main/java/io/theothercompany/theothertodolist/web/AtController.java
+++ src/main/java/io/theothercompany/theothertodolist/web/AtController.java
@@ -5,7 +5,16 @@
  */
 package io.theothercompany.theothertodolist.web;
 
+import io.theothercompany.theothertodolist.service.AtService;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  *
@@ -13,4 +22,21 @@ import org.springframework.stereotype.Controller;
  */
 @Controller
 public class AtController {
+    @Autowired
+    protected AtService atService;
+
+    @RequestMapping(value = "/rest/at/populate.json", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseBody
+    public Map<String, List<Integer>> populateItems() {
+        atService.parse(1, "pears @grocery");
+        atService.parse(2, "Spring @dev");
+        atService.parse(3, "something @test @ignored");
+        return atService.collectTypes();
+    }
+    
+    @RequestMapping(value = "/rest/at/list.json", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseBody
+    public Map<String, List<Integer>> getTodoItems() {
+        return atService.collectTypes();
+    }
 }
